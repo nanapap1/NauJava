1ef0536c2308a315c1384bdf537d0924e43fa98a
nanapap1 <markmoskvitin@MacBook-Pro-Mark.local>
2025-10-27 17:21:55 +0300

    Delete Service

--- src/main/java/ru/MarkMoskvitin/NauJava/service/Services.java
+++
@@ -1,9 +0,0 @@
-package ru.MarkMoskvitin.NauJava.service;
-
-public interface Services<T>
-{
-    void updateDescription(Long id, String descr);
-    void createTask(Long id, String description);
-    T findById(Long id);
-    void deleteById(Long id);
-}
