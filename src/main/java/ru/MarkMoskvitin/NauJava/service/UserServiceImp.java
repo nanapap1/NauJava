8e722aa3a7668e0a92c2f4e7795598d262bfc51a
nanapap1 <markmoskvitin@MBP-Mark.Dlink>
2025-11-02 15:30:43 +0300

    Implement UserDetailsService

--- src/main/java/ru/MarkMoskvitin/NauJava/service/UserServiceImp.java
+++ src/main/java/ru/MarkMoskvitin/NauJava/service/UserServiceImp.java
@@ -2,18 +2,28 @@ package ru.MarkMoskvitin.NauJava.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 import ru.MarkMoskvitin.NauJava.entity.User;
+import ru.MarkMoskvitin.NauJava.models.Role;
 import ru.MarkMoskvitin.NauJava.repo.TaskRepository;
 import ru.MarkMoskvitin.NauJava.repo.UserRepository;
 import ru.MarkMoskvitin.NauJava.entity.Task;
+
+import java.util.Collection;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
-public class UserServiceImp implements UserService {
+public class UserServiceImp implements UserService, UserDetailsService {
     private final TaskRepository taskRepository;
     private final UserRepository userRepository;
     private final PlatformTransactionManager transactionManager;
@@ -44,5 +54,16 @@ public class UserServiceImp implements UserService {
             transactionManager.rollback(status);
             throw ex;
         }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) throw new UsernameNotFoundException("User not found");
+        return new org.springframework.security.core.userdetails.User(user.getUsername(),user.getPassword(),mapRoles(user.getRoles()));
+    }
+
+    private Collection<? extends GrantedAuthority> mapRoles(Set<Role> roles){
+        return roles.stream().map(role -> new SimpleGrantedAuthority("ROLE_" + role.name())).collect(Collectors.toSet());
     }
 }
