6ae4510f7a3a5289d097c0ab622cc40d94b10531
nanapap1 <markmoskvitin@MBP-Mark.Dlink>
2025-11-02 14:43:58 +0300

    Add homepage

---
+++ src/main/java/ru/MarkMoskvitin/NauJava/controller/MainController.java
@@ -0,0 +1,14 @@
+package ru.MarkMoskvitin.NauJava.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String showMainPage()
+    {
+        return "main";
+    }
+}
