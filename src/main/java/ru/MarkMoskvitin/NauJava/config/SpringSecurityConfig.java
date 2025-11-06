a983f559b8c30ba611c6f1f2ac2215e17a19d113
nanapap1 <markmoskvitin@MBP-Mark.Dlink>
2025-11-06 23:16:12 +0300

    Add SecurityFilterChain bean

--- src/main/java/ru/MarkMoskvitin/NauJava/config/SpringSecurityConfig.java
+++ src/main/java/ru/MarkMoskvitin/NauJava/config/SpringSecurityConfig.java
@@ -2,9 +2,14 @@ package ru.MarkMoskvitin.NauJava.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import ru.MarkMoskvitin.NauJava.models.Role;
+import ru.MarkMoskvitin.NauJava.repo.UserRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -12,5 +17,17 @@ public class SpringSecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
+        return security
+                .authorizeHttpRequests(
+                auth -> auth.
+                        requestMatchers("/register").permitAll()
+                        .requestMatchers("/swagger-ui/index.html").hasRole(Role.ADMIN.name())
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                .build();
     }
 }
