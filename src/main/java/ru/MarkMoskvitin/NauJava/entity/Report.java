7e6a0195dd3a9d9e08d2a87a3a5940ae669dbc62
nanapap1 <markmoskvitin@MBP-Mark.localnet>
2025-11-13 15:13:14 +0300

    Add service

--- src/main/java/ru/MarkMoskvitin/NauJava/entity/Report.java
+++ src/main/java/ru/MarkMoskvitin/NauJava/entity/Report.java
@@ -14,7 +14,39 @@ public class Report {
     @Column
     private StatusReport status;
     @Column
+    private String content_tasks;
+    @Column
     private String content;
+    @Column
+    private Long thread1Time;
+    @Column
+    private Long thread2Time;
+    @Column
+    private Long executionTime;
+
+    public Long getExecutionTime() {
+        return executionTime;
+    }
+
+    public void setExecutionTime(Long executionTime) {
+        this.executionTime = executionTime;
+    }
+
+    public Long getThread1Time() {
+        return thread1Time;
+    }
+
+    public void setThread1Time(Long thread1Time) {
+        this.thread1Time = thread1Time;
+    }
+
+    public Long getThread2Time() {
+        return thread2Time;
+    }
+
+    public void setThread2Time(Long thread2Time) {
+        this.thread2Time = thread2Time;
+    }
 
     public String getContent() {
         return content;
@@ -38,5 +70,13 @@ public class Report {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getContent_tasks() {
+        return content_tasks;
+    }
+
+    public void setContent_tasks(String content_tasks) {
+        this.content_tasks = content_tasks;
     }
 }
--- src/main/java/ru/MarkMoskvitin/NauJava/repo/ReportRepository.java
+++ src/main/java/ru/MarkMoskvitin/NauJava/repo/ReportRepository.java
@@ -1,7 +1,9 @@
 package ru.MarkMoskvitin.NauJava.repo;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 import ru.MarkMoskvitin.NauJava.entity.Report;
-
+@RepositoryRestResource(path = "path")
 public interface ReportRepository extends CrudRepository<Report,Long> {
 }
